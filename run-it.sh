335f109eabc42279d1207c7b64ac384614237162
ndreey <andbou95@gmail.com>
2025-06-01 20:44:10 +0200

    some tweaks

--- run-it.sh
+++ run-it.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name nextflow
-#SBATCH -A naiss2024-22-580
+#SBATCH -A naiss2025-22-494
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -c 2
 #SBATCH --mem=5GB
-#SBATCH -t 08:30:00
+#SBATCH -t 6-23:00:00
 #SBATCH --output=slurm-logs/nf-SLURM-%j.out
 #SBATCH --error=slurm-logs/nf-SLURM-%j.err
 #SBATCH --mail-user=andbou95@gmail.com
@@ -18,4 +18,4 @@ ml load nextflow/24.04.2
 
 
 
-nextflow run main.nf -params-file stam-params.yml 
+nextflow run main.nf -params-file stam-params.yml -profile slurm
