616ccf61faf92401f4360b196db375295586baed
ndreey <andbou95@gmail.com>
2025-06-03 20:56:23 +0200

    Tweaked so everything until merger seemed to have worked

--- run-it.sh
+++ run-it.sh
@@ -12,10 +12,11 @@
 #SBATCH --mail-user=andbou95@gmail.com
 #SBATCH --mail-type=ALL
 
-ml load PDC/23.12 
-ml load apptainer/1.3.6-cpeGNU-23.12
+#ml load PDC/23.12 
+#ml load apptainer/1.3.6-cpeGNU-23.12
+
+ml load PDC/24.11
+ml load apptainer/1.4.0-cpeGNU-24.11
 ml load nextflow/24.04.2
-
-
 
 nextflow run main.nf -params-file stam-params.yml -profile slurm -resume
